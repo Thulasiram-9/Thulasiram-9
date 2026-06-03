84dc72c15606dac69fe5a1305decb8fbdece4116
Thulasiram-9 <thulasiguru2006@gmail.com>
2026-06-03 20:58:27 +0530

    hello.py updated to say hi in three different languages

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("vanakkam Yazhpanam")
+lang=input("What is your native language? ")
+if lang=="english":
+    print("Hello!")
+elif lang=="Tamil":
+    print("Vanakkam!")
+elif lang=="Sinhala":
+    print("Ayubowan!")
+else:
+    print("I'm excited to learn your language too!")
